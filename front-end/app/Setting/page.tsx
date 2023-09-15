fecf2319bc146b55ea64f21aa2c8ce56c91040ec
IMAD ABID <imabid@student.1337.ma>
2023-09-15 22:18:52 +0100

    LANDING PAGE

--- front-end/app/Setting/page.tsx
+++ front-end/app/Setting/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Page() {
   return (
-    <div className='h-[100vh] w-[100vw] flex '>
+    <div className='h-[100vh] w-[100vw] flex'>
       here is the setting page
+      <div className='bg-[#fff]  xs:bg-black sm:bg-blue-300'>  
+        hello world
+      </div>
     </div>
   )
 }
