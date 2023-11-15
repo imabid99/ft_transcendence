483a6b981c15cff60ffd8d62c65d6313969dfd68
imabid99 <imabid@student.1337.ma>
2023-11-15 19:37:30 +0100

    Add lottie animation : Home page

--- front-end/components/Dashboard/Profile/Achievements/images.tsx
+++ front-end/components/Dashboard/Profile/Achievements/images.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import Image from 'next/image'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Loading from '@/app/loading';
 
-interface ImageProps {
-    src: string;
-    alt: string;
-    className?: string;
-}
+type ImageType = {
+  src: string;
+  alt: string;
+  className?: string;
+};
 
-interface ImageGridProps {
-    images: ImageProps[][];
-}
+type ImageComponentProps = {
+  images: ImageType[][];
+};
 
-const ImageGrid: React.FC<ImageGridProps> = ({ images }) => {
-    return (
-      <div className="flex items-center flex-col w-12/12 gap-[30px] lg:gap-0 pb-[30px]">
-        {images.map((imageRow, rowIndex) => (
-          <div key={rowIndex} className="flex items-center justify-center gap-[30px] flex-col sm:flex-row">
-            {imageRow.map((img, imgIndex) => (
-              <div key={imgIndex} className="transform hover:scale-110 transition-transform duration-300 relative">
-                <Image src={img.src} alt={img.alt} className={img.className} width={197}
-              height={220}/>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
+function ImageComponent({ images }: ImageComponentProps) {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
   };
 
-export default ImageGrid;
+  return (
+    <div className="flex items-center flex-col w-12/12 gap-[30px] lg:gap-0 pb-[30px]">
+      {loading && <Loading />}
+      {images.map((imageRow, rowIndex) => (
+        <div key={rowIndex} className="flex items-center justify-center gap-[30px] flex-col sm:flex-row">
+          {imageRow.map((img, imgIndex) => (
+            <div key={imgIndex} className="transform hover:scale-110 transition-transform duration-300 relative">
+              <Image src={img.src} alt={img.alt} className={img.className} width={197} height={220} onLoad={handleImageLoad} loading="lazy"/>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ImageComponent;
