3edc337468e8c9b5736fa64f3777147a0351d620
imabid99 <imabid@student.1337.ma>
2023-11-17 21:54:02 +0100

    ADD: map and mode Components to The game

--- front-end/app/Game/page.tsx
+++ front-end/app/Game/page.tsx
@@ -1,15 +1,50 @@
 'use client';
 import Modes from "@/components/Dashboard/Game/Mode/Modes";
-
-// map = snow, desert, forest; mode = friend, bot, random
+import { useState, useEffect } from "react";
+import Maps from "@/components/Dashboard/Game/Map/Maps";
+import { useRouter } from 'next/navigation';
 
 const Game = () => {
+    const [Show, setShow] = useState<string | null>(null);
+    const [selectedMap, setSelectedMap] = useState<string | null>(null);
+    const [selectedMode, setSelectedMode] = useState<string | null>(null);
+    const router = useRouter();
+
+    const handleMapChange = (map: string) => {
+        setSelectedMap(map);
+    }
+
+    const handleModeChange = (mode: string) => {
+        setSelectedMode(mode);
+    }
+
+    console.log("this is map",selectedMode);
 
 	// 7ett dakchi dyalk hna a Imad
 		
 	return (
+		<>
+			<div className="absolute top-0 left-0 w-full h-full  overflow-hidden">
+				<video
+						src="Europa Official Game Reveal Trailer.mp4"
+						autoPlay={true}
+						muted={true}
+						loop={true}  
+						className="h-full object-cover w-full"
+						controls={false}
+						disablePictureInPicture={false}
+						data-wf-ignore={true}
+					/>
+			</div>
+			{
+				Show == null ? <Modes setShow={setShow} onModeChange={handleModeChange} />
+				:
+				Show == 'map' && <Maps setShow={setShow} onMapChange={handleMapChange} />
+				// :
+				// Show == 'slect' && <Select />
+			}
+		</>
 		// <div className="w-full relative">
-		<Modes />
 		// </div>
 	);
 };
--- front-end/components/Dashboard/Game/Map/Maps.tsx
+++ front-end/components/Dashboard/Game/Map/Maps.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import {useState} from 'react';
 // type props  = {
 //     name: string,
 //     username: string,
@@ -8,76 +9,86 @@ import Link from "next/link";
 //     online: string
 //     userId: string
 // }
+type props  = {
+    setShow: any,
+    onMapChange: any
+}
 
-const Maps = () => {
-
+const Maps = ({setShow, onMapChange}:props) => {
+    const [selected, setSelected] = useState('');
+    const handleClick = (map:string) => {
+  setSelected(map);
+  onMapChange(map);
+}
     return (
-<>
-    <div className="absolute top-0 left-0 w-full h-full  overflow-hidden">
-        <video
-                src="Europa Official Game Reveal Trailer.mp4"
-                autoPlay={true}
-                muted={true}
-                loop={true}  
-                className="h-full object-cover w-full"
-                controls={false}
-                disablePictureInPicture={false}
-                data-wf-ignore={true}
-            />
-    </div>
-    <div className="w-[1370px] xl:h-[830px] rounded-[100px] backdrop-blur-[20px] backSh flex gap-[72px] flex-col">
-        <div className="w-full flex justify-center pt-[24px]">
-        <img src="king.svg" alt="" />
-        </div>
-        <div className="flex gap-[20px] justify-center flex-col xl:flex-row items-center">
-        <div className="flex gap-[20px] flex-col lg:flex-row items-center">
-            <div className="w-[372px] min-h-[201px] rounded-[14px] aiSh relative overflow-hidden ">
-            <img
-                src="forest.svg"
-                alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
-            />
-            <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
-                FOREST
-            </p>
-            <div className="dark-overlay" />
+        <>
+        {/* <div className="absolute top-0 left-0 w-full h-full  overflow-hidden">
+            <video
+                    src="Europa Official Game Reveal Trailer.mp4"
+                    autoPlay={true}
+                    muted={true}
+                    loop={true}  
+                    className="h-full object-cover w-full"
+                    controls={false}
+                    disablePictureInPicture={false}
+                    data-wf-ignore={true}
+                />
+        </div> */}
+        <div className="w-[1370px] xl:h-[830px] rounded-[100px] backdrop-blur-[20px] backSh flex gap-[72px] flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="w-full flex justify-center pt-[24px]">
+            <img src="king.svg" alt="" />
             </div>
-            <div className="w-[372px] h-[201px] rounded-[14px] aiSh relative overflow-hidden">
-            <img
-                src="desert.svg"
-                alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
-            />
-            <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
-                DESERT
-            </p>
-            <div className="dark-overlay" />
+            <div className="flex gap-[20px] justify-center flex-col xl:flex-row items-center">
+            <div className="flex gap-[20px] flex-col lg:flex-row items-center">
+                <div className={`w-[372px] min-h-[201px] rounded-[14px] aiSh relative overflow-hidden cursor-pointer ${selected === 'forest' ? 'border-4 border-white backdrop-blur-[20px] bg-black' : ''}`} onClick={() => handleClick('forest')}>
+                <img
+                    src="forest.svg"
+                    alt=""
+                    className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
+                />
+                <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
+                    FOREST
+                </p>
+                <div className="dark-overlay" />
+                </div>
+                <div className={`w-[372px] h-[201px] rounded-[14px] aiSh relative overflow-hidden cursor-pointer ${selected === 'desert' ? 'border-4 border-white backdrop-blur-md'  : ''}`}
+            onClick={() => handleClick('desert')}>
+                <img
+                    src="desert.svg"
+                    alt=""
+                    className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
+                />
+                <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
+                    DESERT
+                </p>
+                <div className="dark-overlay" />
+                </div>
+            </div>
+            <div>
+                <div className={`w-[372px] h-[201px] rounded-[14px] aiSh relative overflow-hidden cursor-pointer ${selected === 'snow' ? 'border-4 border-white backdrop-blur-md' : ''}`}
+            onClick={() => handleClick('snow')}>
+                <img
+                    src="snow.svg"
+                    alt=""
+                    className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
+                />
+                <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
+                    SNOW
+                </p>
+                <div className="dark-overlay" />
+                </div>
+            </div>
+            </div>
+            <div className="w-full  flex justify-center gap-[10px] pb-[40px] flex-col items-center md:flex-row">
+            <button onClick={()=>{setShow(null)}} className="w-[240px] h-[77px] retB rounded-[14px] text-white text-[30px] font-[400] hover:bg-gray-400 font-['Fredoka']">
+                Back
+            </button>
+            <button disabled={!selected} onClick={()=>setShow("map")} className={`w-[240px] h-[77px] backB rounded-[14px] text-white text-[30px] font-[400] hover:bg-gray-400 font-['Fredoka'] ${!selected ? 'cursor-not-allowed text-[20px]' : ''}`}>
+                {selected ? 'Next' : 'Choose Map'}
+            </button>
             </div>
         </div>
-        <div>
-            <div className="w-[372px] h-[201px] rounded-[14px] aiSh relative overflow-hidden">
-            <img
-                src="snow.svg"
-                alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover  z-0 blur-[0.5px]"
-            />
-            <p className="absolute  backdrop-blur-[0.5px] text-center text-white font-[600] text-[40px] z-10 top-[120px] hover-move w-full h-full font-['Fredoka']">
-                SNOW
-            </p>
-            <div className="dark-overlay" />
-            </div>
-        </div>
-        </div>
-        <div className="w-full  flex justify-center gap-[10px] pb-[40px] flex-col items-center md:flex-row">
-        <button className="w-[240px] h-[77px] retB rounded-[14px] text-white text-[30px] font-[400] hover:bg-gray-400 font-['Fredoka']">
-            Back
-        </button>
-        <button className="w-[240px] h-[77px] backB rounded-[14px] text-white text-[30px] font-[400] hover:bg-gray-400 font-['Fredoka']">
-            Next
-        </button>
-        </div>
-    </div>
-</>
+    </>
 
 
 );
