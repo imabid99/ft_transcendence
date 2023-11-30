3beafeaad6dfe1fbe5506e27d5f8f070d12cf9b1
NeoMood <saad02.gmira2002@gmail.com>
2023-11-30 18:08:50 +0100

    Added a button to exit the game 2

--- front-end/app/Game/random/layout.tsx
+++ front-end/app/Game/random/layout.tsx
@@ -1,7 +1,7 @@
 import "../../globals.css";
 import Head from "next/head";
 import Dashboard from '@/components/Dashboard/Dashboard/Dashboard'
-import Link from "next/link";
+import ExitGame from "@/components/Dashboard/Game/Exit_game/exit_game";
 
 export const metadata = {
   title: "Inbox",
@@ -24,10 +24,7 @@ export default function ChatLayout({
       </Head>
         <div className="h-[100vh] w-[100vw] flex bg-[#FAFDFF]">
           <Dashboard path='Game'/>
-          <Link href={"/Game"} className="fixed bg-white transition ease-in-out duration-400 hover:bg-[#064A85] rounded-[16px] w-[55px] h-[55px] top-3 right-2 z-10">
-            <img src={`/exit_game.svg`} alt="" className="mr-4"/>
-            <span className="absolute w-full h-full top-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200 text-white font-bold">exit?</span>
-          </Link>
+          <ExitGame/>
           {children}
         </div>
     </>
