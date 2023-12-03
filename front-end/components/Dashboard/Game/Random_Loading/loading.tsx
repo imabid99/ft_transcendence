3f5471047657e60f60b96b5493453833860032a1
imabid99 <imabid@student.1337.ma>
2023-12-03 18:06:38 +0100

    Achievements with a low quality

--- front-end/components/Dashboard/Game/Random_Loading/loading.tsx
+++ front-end/components/Dashboard/Game/Random_Loading/loading.tsx
@@ -1,12 +1,12 @@
 import Lottie from "lottie-react";
-import randomanimationData from "../../../../public/randomanimation1.json";
+import randomanimationData from "../../../../public/handloading.json";
 
 export default function LoadingRandom() {
     return (
         <div className="absolute flex justify-center items-center w-full h-[100vh] bg-gray-50">
-            <div className="w-[500px] h-[500px]">
+
                 <Lottie animationData={randomanimationData} />
-            </div>
+
         </div>
     );
 }
