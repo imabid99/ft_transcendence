98eb62009941fd22ebb95d32b0a85c75a60df61e
imabid99 <imabid@student.1337.ma>
2023-12-04 15:16:09 +0100

    Add search: setting and profile

--- front-end/components/Dashboard/Profile/Header/Header.tsx
+++ front-end/components/Dashboard/Profile/Header/Header.tsx
@@ -12,6 +12,16 @@ type HeaderProps = {
     notifRef?: any,
     notifIconRef?: any
 }
+type ChannelProps = {
+    avatar: string,
+    channel: string,
+    lastMessage: string | null | undefined,
+    lastMessageTime: string | null | undefined,
+    notification: number,
+    active: boolean,
+    link: string,
+}
+
 
 
 function UserComponent({avatar, channel, lastMessage, lastMessageTime, notification, active, link}: ChannelProps) {
@@ -51,7 +61,6 @@ export default function Header({show, modalRef,handelShaw,notifRef,notifIconRef}
 	const handleSearch = (e: any) => {
         setCloseSearch(false)
         setShowBody(true)
-        console.log("hereee : ", users)
 		const value = e.target.value;
 		const result = profiles.filter((profile: any) => {
 			if (profile.userId === user.id) return false;
@@ -67,7 +76,7 @@ export default function Header({show, modalRef,handelShaw,notifRef,notifIconRef}
 	};
     return (
         <div className="flex items-center h-[100px] w-full  justify-center pt-[100px]
-        
+        z-20
         ">
                 <div className='header__right flex  h-[90px] items-center justify-between   gap-[10px]  px-[30px] lg:px-[20px] xl:px-0 w-[922px] 3xl:w-[1884px] '>
                     <div className='flex gap-[30px]  w-full'>
@@ -75,11 +84,9 @@ export default function Header({show, modalRef,handelShaw,notifRef,notifIconRef}
                             <p className="text-[18px] font-[500] text-[#AEBAC7]  2xl:max-3xl:text-[15px]">Welcome</p>
                             {name ? <p className="text-[25px] font-[700] 2xl:max-3xl:text-[20px]  ">{name}</p> : <p className="text-[25px] font-[700] 2xl:max-3xl:text-[20px]  "></p>}
                         </div>
-                        <div className="flex  gap-[20px] items-center z-[0] sSha rounded-[20px] bg-white w-full 3xl:w-[780px] ">
+                        <div className="flex relative  gap-[20px] items-center z-[0] sSha rounded-[20px] bg-white w-full 3xl:w-[780px] ">
                             <div className="flex items-center gap-[10px] p-[32px] pr-0  ">
-                                {/* <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M27 27L21.0974 21.0868M24.3684 13.1842C24.3684 16.1504 23.1901 18.9952 21.0926 21.0926C18.9952 23.1901 16.1504 24.3684 13.1842 24.3684C10.218 24.3684 7.37323 23.1901 5.27578 21.0926C3.17833 18.9952 2 16.1504 2 13.1842C2 10.218 3.17833 7.37323 5.27578 5.27578C7.37323 3.17833 10.218 2 13.1842 2C16.1504 2 18.9952 3.17833 21.0926 5.27578C23.1901 7.37323 24.3684 10.218 24.3684 13.1842Z" stroke="#B4C0CB"/>
-                                </svg>  */}
+
                                 {
                                     closeSearch ? (
                                         <span onClick={() => { setCloseSearch(false); inputRef.current?.focus(); }} className="cursor-pointer">
@@ -115,7 +122,7 @@ export default function Header({show, modalRef,handelShaw,notifRef,notifIconRef}
                             <input ref={inputRef} onChange={handleSearch} className="w-full h-[90px] bg-[#FFF]  rounded-[10px] outline-none border-none text-[20px] text-gray-400 font-[500] font-[Poppins] pr-[20px] placeholder:font-[400] placeholder:text-gray-300 " type="text" placeholder="Search for friends to play" />
                             {
                                     showBody && (
-                                        <div className=" z-[50] absolute top-[100px] w-full rounded-[15px] h-[280px] bg-[#FFF] py-[25px] px-[25px] flex flex-col gap-[20px] overflow-y-scroll no-scrollbar p-inf">
+                                        <div className=" z-[500] absolute top-[100px] w-full rounded-[15px] h-[280px] bg-[#FFF] py-[25px] px-[25px] flex flex-col gap-[20px] overflow-y-scroll no-scrollbar p-inf">
                                             {
                                                 users?.map((user: any) => {
                                                     return(
